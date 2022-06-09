6b910e285fcd6e01d5482c1ef20b2c88faa54eca
Jackb01 <joceranb1@gmail.com>
2022-06-09 03:39:17 +0200

    Home B process 70%

--- src/pages/homeb.js
+++ src/pages/homeb.js
@@ -1,7 +1,27 @@
-import BottomNav from "../components/BottomNav";
+import React from 'react';
+import { Button, Typography, Avatar, Grid } from '@mui/material';
 
 export default function HomeB(){
     return (
-        <BottomNav></BottomNav>
+        <>
+                 <br /><br />
+                <Avatar
+                    alt ="Rema Logo"
+                    sx={{width: 90, height: 90, margin: "auto", marginBottom: 30}}>
+                        Rema 
+                </Avatar>
+                <br /><br />
+
+                <Grid container spacing={{xs: 8}} columns={{xs: 1}} direction="column" alignItems="center" justifyContent="center">
+                    <Grid item xs={2} sm={4}>
+                        <Typography variant="h5" component="h5">
+                            Welcome To Rema 1000 Shop
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={2} sm={4}>
+                        <Button variant="contained" size="large">Start Shopping</Button>
+                    </Grid>
+                </Grid>
+        </>
     )
 }
